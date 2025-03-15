0a33b80192d846ff204a492b8d23e1b2e9b67794
Codebox124 <rajcodes733@gmail.com>
2025-03-15 15:39:08 +0100

    updated

---
+++ data/data.tsx
@@ -0,0 +1,320 @@
+// data/portfolioProjects.ts
+export const portfolioProjects: Record<
+    string,
+    { title: string; description: string; image: string  }[]
+> = {
+    "website-design": [
+        {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/blog 3.jpg",
+        },
+        {
+            title: "Portfolio Website",
+            description: "Built a personal portfolio website for a creative artist.",
+            image: "/website/caymana.co Landing Page edit.png",
+        },
+        {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/desktop full page update.png",
+        },
+        {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/port web Moses Villani.png",
+        },
+        {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/shifto 4.png",
+        },
+        {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/shifto 6.png",
+        },
+        {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/shifto 7.png",
+        },
+        {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/site port.jpg",
+        }, {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/web port vibal (2).PNG",
+        }, {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/website 2.png",
+        }, 
+        {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/website port 2.png",
+        },
+        {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/website port 4.png",
+        },
+        {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/website port.png",
+        },
+        {
+            title: "E-commerce Website",
+            description: "Designed and developed a fully responsive online store.",
+            image: "/website/website.png",
+        },
+    ],
+    "photo-editing": [
+        {
+            title: "Food Delivery App",
+            description: "Developed a cross-platform food delivery application.",
+            image: "/photo/_1260573 edit.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/_1260944 edit.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/DSC00327 edit.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/DSC00342 edit.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/IMG-5831.jpg",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/music pics 2.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/photo [Lisa Kinzer] AFTER.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/pic.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/pic2.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/pic3.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/pic4.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/pic5.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/pic6.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/pic7.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/pic8.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/pic9.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/pic10.png",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/portfolio - photo colorGRADE00.jpg",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/portfolio - photo colorGRADE01.jpg",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/portfolio - photo colorGRADE02.jpg",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/portfolio - photo colorGRADE04.jpg",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/portfolio - photo colorGRADE07.jpg",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/portfolio - photo colorGRADE10.jpg",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/portfolio - photo colorGRADE13.jpg",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/portfolio - photo colorGRADE15.jpg",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/portfolio - photo colorGRADE17.jpg",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/portfolio - photo colorGRADE19.jpg",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/portfolio - photo colorGRADE24.jpg",
+        },
+        {
+            title: "Health & Fitness Tracker",
+            description: "Designed an app to track fitness and health routines.",
+            image: "/photo/portfolio - photo colorGRADE25.jpg",
+        },
+    ],
+    "graphic-design": [
+        {
+            "title": "Tech Startup Brand Identity",
+            "description": "Designed a full branding package including logo, color palette, and typography for a tech startup.",
+            "image": "/graphic/3.png"
+        },
+        {
+            "title": "Modern Logo & Branding",
+            "description": "Created a sleek and modern logo with a cohesive branding theme for a startup.",
+            "image": "/graphic/4.png"
+        },
+        {
+            "title": "Visual Identity System",
+            "description": "Developed a consistent brand identity system for a growing company, including logo variations.",
+            "image": "/graphic/Moses.jpg"
+        },
+        {
+            "title": "Bold & Edgy Logo Design",
+            "description": "Designed a striking logo with a rebellious theme for a unique brand.",
+            "image": "/graphic/bandit.png"
+        },
+        {
+            "title": "Professional Business Card Design",
+            "description": "Created an eye-catching and professional business card for enhanced networking impact.",
+            "image": "/graphic/biz card.png"
+        },
+        {
+            "title": "Event Flyer for Bonefish",
+            "description": "Designed a visually engaging promotional flyer for an exclusive event.",
+            "image": "/graphic/bonefish flyer 4 MIKE.png"
+        },
+        {
+            "title": "Custom Book Cover Design",
+            "description": "Designed a bold and captivating book cover to enhance market appeal.",
+            "image": "/graphic/book cover 3 KING.png"
+        },
+        {
+            "title": "Corporate Brochure Design",
+            "description": "Developed an informative and well-structured brochure for a corporate brand.",
+            "image": "/graphic/brochure edit pg2.jpeg"
+        },
+        {
+            "title": "Certificate Design",
+            "description": "Designed an elegant and professional certificate for official recognition.",
+            "image": "/graphic/cert SAMPLE 2.png"
+        },
+        {
+            "title": "Certificate Design",
+            "description": "Designed an elegant and professional certificate for official recognition.",
+            "image": "/graphic/cover art ROBERT FORD edit 1.5"
+        },
+        {
+            "title": "Certificate Design",
+            "description": "Designed an elegant and professional certificate for official recognition.",
+            "image": "/graphic/flyer - Michael_Rebecca EDIT 1.png"
+        },
+        {
+            "title": "Certificate Design",
+            "description": "Designed an elegant and professional certificate for official recognition.",
+            "image": "/graphic/flyer 2.png"
+        },
+        {
+            "title": "Certificate Design",
+            "description": "Designed an elegant and professional certificate for official recognition.",
+            "image": "/graphic/flyer 3 SAL.jpg"
+        },
+        {
+            "title": "Certificate Design",
+            "description": "Designed an elegant and professional certificate for official recognition.",
+            "image": "/graphic/flyer 3 SAL.jpg"
+        },
+        {
+            "title": "Certificate Design",
+            "description": "Designed an elegant and professional certificate for official recognition.",
+            "image": "/graphic/flyer OLA CD 2.png"
+        },
+        {
+            "title": "Certificate Design",
+            "description": "Designed an elegant and professional certificate for official recognition.",
+            "image": "/graphic/flyer PHIL edit.png"
+        },
+        {
+            "title": "Certificate Design",
+            "description": "Designed an elegant and professional certificate for official recognition.",
+            "image": "/graphic/Flyer Who Let The Dogs Out Fashion Show.png"
+        },
+        
+    ],
+
+    "video-editing": [
+        {
+            title: "Dynamic Promo Video",
+            description: "A high-energy promotional video for a new product launch.",
+            image: "/photo/video portfolio.mp4",
+          },
+    ]
+
+};
