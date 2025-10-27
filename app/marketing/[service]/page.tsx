7ef28293d7c1c73e1af48fe260776f2607368219
carlden10 <carlden10@gmail.com>
2025-10-27 17:28:41 +0700

    canonical

--- app/marketing/[service]/page.tsx
+++ app/marketing/[service]/page.tsx
@@ -1,15 +1,21 @@
 import { portfolioMarketingProjects, serviceDescriptions, serviceTitles } from "@/data/data";
 import PortfolioClient from "./PortfolioClient";
+import { notFound } from "next/navigation";
+
 
 export async function generateMetadata({ params }: { params: Promise<{ service: string }> }) {
   const resolvedParams = await params;
   const service = resolvedParams.service?.replace(/-/g, " ") ?? "";
-  const title =`${serviceTitles[resolvedParams.service]}`
+  const title =`${serviceTitles[resolvedParams.service]}`;
   const description = serviceDescriptions[resolvedParams.service] || "Discover our work in this category.";
+  const canonicals = `https://yourdomain.com/marketing/${resolvedParams.service}`;
 
   return {
     title,
     description,
+    alternates: {
+      canonical: canonicals
+    },
     openGraph: {
       title,
       description,
@@ -18,6 +24,13 @@ export async function generateMetadata({ params }: { params: Promise<{ service: 
   };
 }
 
+const validServices = [
+  "website-and-apps-design",
+  "advertising",
+  "social-media",
+  "flyer-distribution",
+];
+
 export default async function Page({ params }: { params: Promise<{ service: string }> }) {
   const resolvedParams = await params;
   const service = Array.isArray(resolvedParams.service)? resolvedParams.service.join("-"): resolvedParams.service ?? "";
@@ -25,6 +38,9 @@ export default async function Page({ params }: { params: Promise<{ service: stri
   const formattedService = service.replace(/-/g, " ");
   const descriptionservice = serviceDescriptions[service] || "Discover our work in this category.";
   const projects = portfolioMarketingProjects[service] || [];
+  if (!validServices.includes(resolvedParams.service)) {
+    notFound();
+  }
 
   return (
     <PortfolioClient
--- app/visuals/[service]/page.tsx
+++ app/visuals/[service]/page.tsx
@@ -1,15 +1,21 @@
 import { portfolioVisualProjects, serviceDescriptions, serviceTitles } from "@/data/data";
 import PortfolioClient from "./PortfolioClient";
+import { notFound } from "next/navigation";
+
 
 export async function generateMetadata({ params }: { params: Promise<{ service: string }> }) {
   const resolvedParams = await params;
   const service = resolvedParams.service?.replace(/-/g, " ") ?? "";
   const title =`${serviceTitles[resolvedParams.service]}`
   const description = serviceDescriptions[resolvedParams.service] || "Discover our work in this category.";
+  const canonicals = `https://yourdomain.com/marketing/${resolvedParams.service}`;
 
   return {
     title,
     description,
+    alternates: {
+      canonical: canonicals
+    },
     openGraph: {
       title,
       description,
@@ -18,6 +24,13 @@ export async function generateMetadata({ params }: { params: Promise<{ service: 
   };
 }
 
+const validServices = [
+  "graphic-design",
+  "video-editing",
+  "photo-editing",
+  "animation",
+];
+
 export default async function Page({ params }: { params: Promise<{ service: string }> }) {
   const resolvedParams = await params;
   const service = Array.isArray(resolvedParams.service)? resolvedParams.service.join("-"): resolvedParams.service ?? "";
@@ -25,6 +38,9 @@ export default async function Page({ params }: { params: Promise<{ service: stri
   const formattedService = service.replace(/-/g, " ");
   const descriptionservice = serviceDescriptions[service] || "Discover our work in this category.";
   const projects = portfolioVisualProjects[service] || [];
+  if (!validServices.includes(resolvedParams.service)) {
+    notFound();
+  }
 
   return (
     <PortfolioClient
