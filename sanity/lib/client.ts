3e373cee11dd8053e97ee5838c019a7ea03ed4b7
carlden10 <carlden10@gmail.com>
2025-12-05 02:14:26 +0700

    add blog sanity

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,12 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
+// PENTING: Import variabel dari file '../env' yang sudah kita hardcode
+import { apiVersion, dataset, projectId, useCdn } from '../env'
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn,
+  perspective: 'published',
 })
