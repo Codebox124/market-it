2b2e94f10df30b4156fa40c120f6b79e28c456c2
carlden10 <carlden10@gmail.com>
2025-12-05 02:28:22 +0700

    fix build sanity

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,12 +1,11 @@
 import { createClient } from 'next-sanity'
 
 // PENTING: Import variabel dari file '../env' yang sudah kita hardcode
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+import {dataset, projectId, useCdn } from '../env'
 
 export const client = createClient({
   projectId,
   dataset,
-  apiVersion,
   useCdn,
   perspective: 'published',
 })
